a634761ee35d1cf02e3269cad27c94a021521101
Joshua Schultz <joshuamschultz@gmail.com>
2020-11-07 20:20:36 -0600

    added demand generator

---
+++ README.rst
@@ -0,0 +1,4 @@
+Inventory Blocks
+================
+
+Inventory blocks is an open source python library aimed at giving the "building blocks" to those wanting to optimize inventory levels.
---
+++ inventory_blocks/__init__.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def generate_demand(mu, sigma, periods, distribution='normal'):
+    if distribution == 'normal':
+        return np.random.normal(mu, sigma, periods)
+    elif distribution == 'lognormal':
+        return np.random.lognormal(mu, sigma, periods)
+    
