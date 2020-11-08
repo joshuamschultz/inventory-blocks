952b811fe2ee4fadbbe7f25dd4b385a51985ed79
Joshua Schultz <joshuamschultz@gmail.com>
2020-11-08 15:15:18 -0600

    finished initial setup

--- setup.py
+++ setup.py
@@ -5,22 +5,6 @@ with open("README.rst", "r") as fh:
 with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
 
-setup(
-    name="inventory_blocks",
-    version="0.0.1",
-    author="Joshua Schultz  ",
-    author_email="joshuamschultz@gmail.com",
-    description="A package to convert your Jupyter Notebook",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/joshuamschultz/inventory-blocks",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
-)
 setuptools.setup(
     name="inventory-blocks",
     version="0.0.1",
@@ -32,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0-only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
