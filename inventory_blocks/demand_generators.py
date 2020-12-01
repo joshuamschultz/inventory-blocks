816d49a047e2ede31ddd6377807a7f164fac7c21
Joshua Schultz <joshuamschultz@gmail.com>
2020-11-30 19:23:39 -0600

    created a class out of the generator

--- inventory_blocks/demand_generators.py
+++ inventory_blocks/demand_generators.py
@@ -1,8 +1,7 @@
-"""Demand Generators
-These methods simulate various types of demand used in
-modeling and optimizing models.
+"""Generators
+This class creates a generator than can be used in simulations.
 
-They can be set using parameters, or can be set 
+Generators are set set using parameters, or can be set 
 by reading actual histories and calculating those
 parameters.
 
@@ -10,32 +9,49 @@ parameters.
 
 import numpy as np
 
-def generate_demand(mu, sigma, periods=1, distribution='normal'):
-    """ generate_demand
+class Generator:
 
-    Parameters
-    ----------
-    mu : float
-        the mean of the demand you would like generated
-        note: for lognormal, this is the is mean of the log observations
-    sigma : float
-        The standard deviation of the demand you would like generated
-        note: for lognormal, this is the standard deviation of the log observations
-    periods : integer, optional
-        How many random points you would like generated
-        (default: 1)
-    distribution : str
-        Which distribution you would like used to generate the demand
-        (default: normal)
-        (options: normal, lognormal)
+    def __init__(self, distribution):
+        """ initialize
 
+        Parameters:
+        distribution: str
+            Which distribution you would like used to generate the demand
+            (default: normal)
+            (options: normal, lognormal, range)
+            note: range changes the functions from mu/sigma to low/high
+        
+        """
+        self.distribution = distribution
+        print(f'Generator initialized as a {self.distribution} distribution ')
 
-    Returns
-    -------
-    array
-        an array of randomly generated demands using the parameters fed
-    """
-    if distribution == 'normal':
-        return np.random.normal(mu, sigma, periods)
-    elif distribution == 'lognormal':
-        return np.random.lognormal(mu, sigma, periods)
+    def generate(self, mu, sigma, periods=1):
+        """ generate
+
+        Parameters
+        ----------
+        mu : float
+            the mean of the demand you would like generated
+            note: for lognormal, this is the is mean of the log observations
+            note: if choosing range, this is the low inclusive of the range, not the mean
+        sigma : float
+            The standard deviation of the demand you would like generated
+            note: for lognormal, this is the standard deviation of the log observations
+            note: if choosing range, this is the high inclusive of the range, not the standard deviation
+        periods : integer, optional
+            How many random points you would like generated
+            (default: 1)
+
+        Returns
+        -------
+        array
+            an array of randomly generated demands using the parameters fed
+        """
+
+        if self.distribution == 'range':
+            return np.random.random_integers(mu, sigma+1, periods)
+        elif self.distribution == 'lognormal':
+            return np.random.lognormal(mu, sigma, periods)
+        elif self.distribution == 'normal':
+            return np.random.normal(mu, sigma, periods)
+        else: return 'No Distribution Specified'
